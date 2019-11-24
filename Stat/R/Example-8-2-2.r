91a7b6608bdc5a2be4963138b092760e7fd4229c
AppliedStat <cpark2@gmail.com>
2019-11-24 18:53:42 +0900

    Engineering Statistics

--- Stat/R/Example-8-2-2.r
+++ Stat/R/Example-8-2-2.r
@@ -1,5 +1,5 @@
 #======================================================
-# 8.2.2 (very similar to 8.2.1 and Exercise 7.2-12)
+# Example 8.2-2 (very similar to 8.2.1 and Exercise 7.2-12)
 #------------------------------------------------------
 x = c(1071, 1076, 1070, 1083, 1082, 1067, 1078, 1080, 1075, 1084, 1075, 1080) 
 y = c(1074, 1069, 1075, 1067, 1068, 1079, 1082, 1064, 1070, 1073, 1072, 1075)
@@ -11,7 +11,8 @@ t.test(x,y, alternative="two.sided", var.equal=TRUE)
 summary(x)
 summary(y)
 
-id = rep( c("X","Y"), c(length(x), length(y)) )
+boxplot(y,x, names=c("Y","X"), horizontal=TRUE )
 
-id3 = factor( id , levels=c("Y", "X") )
-boxplot( c(x,y) ~ id3, horizontal=TRUE )
+# id = rep( c("X","Y"), c(length(x), length(y)) )
+# id3 = factor( id , levels=c("Y", "X") )
+# boxplot( c(x,y) ~ id3, horizontal=TRUE )
