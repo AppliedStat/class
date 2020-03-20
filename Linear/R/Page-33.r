931a59eff212e5e85b90263e6e6ed09079defe43
AppliedStat <cpark2@gmail.com>
2020-03-20 20:44:15 +0900

    Linear

---
+++ Linear/R/Page-33.r
@@ -0,0 +1,13 @@
+
+
+A = matrix( c(1,4,1,7,  0,5,-2,3,  0,0,1,-3), ncol=3)
+
+B = matrix( c(1,-1,0,1, 1,5,-2,1,  0,0,1,-9), ncol=3)
+
+
+# * means  Hadamard product 
+# %*% is a matrix product. 
+A * B
+
+
+
