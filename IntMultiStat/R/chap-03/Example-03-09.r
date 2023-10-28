201cd4eb9a4b388706b11b9bcff0e1d319a64277
AppliedStat <cpark2@gmail.com>
2023-10-28 12:45:04 +0900

    Int. to Multivariate Stat

--- IntMultiStat/R/chap-03/Example-03-09.r
+++ IntMultiStat/R/chap-03/Example-03-09.r
@@ -4,16 +4,18 @@ X = matrix( c(1,4,4, 2,1,0, 5,6,4), ncol=3)
 
 
 xbar = apply(X,2, mean)
+xbar = colMeans(X)
 
-Xminus1xbarprime = X - c(1,1,1)%*%t(xbar)
+
+X.demeaned = X - c(1,1,1)%*%t(xbar)
 
 n = nrow(X)
 
-S = 1/(n-1) * t(Xminus1xbarprime) %*% Xminus1xbarprime 
+S = 1/(n-1) * t(X.demeaned) %*% X.demeaned 
 S
 
 # Or,  we can calcuate S using var() function. 
-S = var(X)
+S = cov(X)
 S 
 
 det(S)
