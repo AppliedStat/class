984831c339afbc191ba021ab7632d3638dbb3bf6
AppliedStat <cpark2@gmail.com>
2023-10-28 15:54:08 +0900

    Int. to Multivariate Stat

--- IntMultiStat/R/chap-03/Example-03-12.r
+++ IntMultiStat/R/chap-03/Example-03-12.r
@@ -8,3 +8,21 @@ X = matrix(c(1,4,4, 2,1,0, 5,6,4), ncol=3)            # Example 3.09 on Page 130
 S = cov(X)
 sum( diag(S) ) 
 
+
+# =======================
+# Extra
+# =======================
+
+# When S = matrix(c(252.04, -68.43, -68.43, 123.67), ncol=2)
+# When S = cov( matrix(c(1,4,4, 2,1,0, 5,6,4), ncol=3) )
+
+# Compare sum( diag(S) ) with sum( eigen(S)$values )
+sum( diag(S) )
+sum( eigen(S)$values )
+
+# Compare det(S) with prod( eigen(S)$values )
+det(S)
+prod( eigen(S)$values )
+
+
+
