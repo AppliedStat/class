d4c1685d6b67722ffe2a89f1068bd7dabb2c2d9f
AppliedStat <cpark2@gmail.com>
2025-04-13 16:41:30 +0900

    공학통계 1 & 2

---
+++ Stat/R/Example-2-5-2.r
@@ -0,0 +1,11 @@
+## ==========================
+## Example 2.5-2 on Page 82 
+## --------------------------
+
+# x = number of white balls
+# m = number of white balls in an urn
+# n = number of black balls in an urn 
+# k = number of balls drawn 
+
+dhyper(x=2, m=10, n=40, k=7) 
+
