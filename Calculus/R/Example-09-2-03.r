409426e7feee055d0803d2ce1fc88e2f49f44d7e
AppliedStat <cpark2@gmail.com>
2025-09-23 18:48:34 +0900

    Calculus

--- Calculus/R/Example-09-2-03.r
+++ Calculus/R/Example-09-2-03.r
@@ -9,3 +9,21 @@ B=  matrix(c(1,2,3,  -4,1,5,  2,3,0), nrow=3, byrow=TRUE)
 det(B)
 
 
+
+# Note 
+
+det(-1)  # 1 is not a matrix
+
+A = matrix(-1, nrow=1) # Now A is a matrix
+A 
+det(A)
+
+
+
+abs(A)
+abs(-1)
+
+
+
+
+
