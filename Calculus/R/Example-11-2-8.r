78a4e2504e0a2b7884d69afe22e171081e63ed56
AppliedStat <cpark2@gmail.com>
2025-11-02 17:21:42 +0900

    Calculus

---
+++ Calculus/R/Example-11-2-8.r
@@ -0,0 +1,42 @@
+
+# Page 457 
+
+# (a)   NB: Example-11-1-2.r on Page 438 
+
+x = seq(-10,10, by=1)
+y = x
+
+f = function(x,y) {sqrt(x^2 + y^2)}
+
+z = array( dim=c(length(x), length(y) ) )
+
+for ( i in 1:length(x) ) { 
+    for ( j in 1:length(y) ) z[i,j] = f(x[i],y[j])
+}
+
+persp(x,y,z)
+contour(x,y,z)
+
+persp(x,y,z, theta=120)  # same as the textbook angle. 
+
+
+#-----------
+
+rm(x,y,z,f)
+
+x = seq(-10,10, by=1)
+y = x
+
+f = function(x,y) { sqrt(x^2 + y^2)}
+z = outer(x,y, f)
+
+
+
+persp(x,y,z)
+contour(x,y,z)
+
+
+par( mfrow=c(1,2) )
+persp(x,y,z, theta=120)  # same as the textbook angle. 
+contour(x,y,z)
+
